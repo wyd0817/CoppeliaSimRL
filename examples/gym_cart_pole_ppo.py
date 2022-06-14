43866c4e07adc3d383b09b0e2ca034ae12498b68
yongdong <pfg46ktw@s.okayama-u.ac.jp>
2022-06-14 22:59:28 +0900

    Change visdom to tensorboard.

--- examples/gym_cart_pole_ppo.py
+++ examples/gym_cart_pole_ppo.py
@@ -6,46 +6,60 @@ import os
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import CallbackList
 from stable_baselines3.common.callbacks import EvalCallback
-
+from os.path import dirname, join, abspath
+from os import path, pardir
+from torch.utils.tensorboard import SummaryWriter
 import sys
-sys.path.append("/Users/wangyongdong/CoppeliaSimRL/utils")
-from callbackFunctions import VisdomCallback
+from datetime import datetime
+import torch
+
+utils_dir = join(abspath(join(dirname(__file__),pardir)),'utils')
+sys.path.append(utils_dir)
+
+tensorboard_log = join(abspath(join(dirname(__file__),pardir)),'logs_train')
 
 # ---------------- create environment
 env = gym.make('CartPole-v1')
 
 # ---------------- callback functions
-log_dir = "/Users/wangyongdong/CoppeliaSimRL/CartPole/saved_models/tmp/PPO"
+log_dir = join(abspath(join(dirname(__file__),pardir)),'CartPole/saved_models/tmp/PPO')
 os.makedirs(log_dir, exist_ok=True)
 env = Monitor(env, log_dir)
 
-callback_visdom = VisdomCallback(name='visdom_simple_cart_pole_rl', check_freq=100, log_dir=log_dir)
 callback_save_best_model = EvalCallback(env, best_model_save_path=log_dir, log_path=log_dir, eval_freq=500, deterministic=True, render=False)
-callback_list = CallbackList([callback_visdom, callback_save_best_model])
+callback_list = CallbackList([callback_save_best_model])
 
+TRAINING_MODE = False
 
 # ---------------- model learning
-# print('Learning the model')
-# model = PPO('MlpPolicy', env, verbose=True)
-# model.learn(total_timesteps=20000, callback=callback_list) # 'MlpPolicy' = Actor Critic Policy
-# print('Learning finished')
+if TRAINING_MODE == True:
+    print('Learning the model')
+    start_time =datetime.now()
+    model = PPO(policy='MlpPolicy',
+            env=env, 
+            learning_rate=7e-4,  
+            policy_kwargs=dict(activation_fn=torch.nn.ReLU, net_arch=[64, 64, 64, 64]),
+            verbose=2, 
+            tensorboard_log = tensorboard_log)
+    model.learn(total_timesteps=20000, callback=callback_list) 
+    end_time =datetime.now()
+    print('The training time: ',(end_time - start_time))
+    print('Learning finished')
 
-# del model
+    del model
+else:
+    # ---------------- prediction
+    print('Prediction')
+    model_dir = join(abspath(join(dirname(__file__),pardir)),'CartPole/saved_models/tmp/PPO/best_model')
+    model = PPO.load(model_dir, env=env)
 
+    observation = env.reset()
+    for i in range(1000):
+        action, _state = model.predict(observation, deterministic=True)
+        observation, reward, done, info = env.step(action)
+        env.render()
+        if done:
+            observation = env.reset()
+        time.sleep(0.01)
 
-# ---------------- prediction
-print('Prediction')
-model = PPO.load("/Users/wangyongdong/CoppeliaSimRL/CartPole/saved_models/tmp/PPO/best_model", env=env)
-
-
-observation = env.reset()
-for i in range(1000):
-    action, _state = model.predict(observation, deterministic=True)
-    observation, reward, done, info = env.step(action)
-    env.render()
-    if done:
-        observation = env.reset()
-
-    time.sleep(0.01)
-
-env.close()
+    env.close()
